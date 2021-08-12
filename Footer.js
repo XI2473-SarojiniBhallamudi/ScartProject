d99b7609f4490f90c845b1f79e6bc0227a05bd15
Sarojini Mythili Bhallamudi <85663086+XI2473-SarojiniBhallamudi@users.noreply.github.com>
2021-08-12 09:33:57 +0530

    Add files via upload
    Header and Footer added

---
+++ Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Footer.css'
+
+function Footer(){
+    return (
+        <div className="footer">
+            <p className="brand-mail">@abc.com</p>
+            <p className="brand-name">sCart</p>
+        </div>
+    );
+}
+
+export default Footer;
